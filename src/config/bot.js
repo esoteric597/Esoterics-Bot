bf0c8232fc4efe924843bab10508bd4890832d2e
esoteric597 <picky.ks@gmail.com>
2026-04-05 02:13:19 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -12,7 +12,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "online",
+    status: "dnd",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Listening to Soli Yap 🙄",
+        name: "SOS SAVE ME FROM GABY! 🥹",
         // Activity type number (0 = Playing).
-        type: 2, 
+        type: 4, 
       },
     ],
   },
