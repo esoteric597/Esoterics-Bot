b0c7994608815545053cf5279a946ce56195b582
esoteric597 <picky.ks@gmail.com>
2026-04-04 23:56:47 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Serving 1,000+ members",
+        name: "FREAK YOU SOLI FOR BREAKING ME 😡😤😒😠🤬👿🗯️💢",
         // Activity type number (0 = Playing).
         type: 4, 
       },
